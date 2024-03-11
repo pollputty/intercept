a38511c1c967d3b0cad2b3d2b077e40e751437ac
Paul PETIT <p@ulpet.it>
2024-03-11 20:41:42 +0100

    run a command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use clap::Parser;
+use tracing::{debug, span, Level};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about=None)]
+struct Args {
+    #[arg(short, long)]
+    debug: bool,
+    #[arg(last = true)]
+    cmd: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    // Init logging
+    tracing_subscriber::fmt()
+        .with_max_level(if args.debug {
+            Level::DEBUG
+        } else {
+            Level::INFO
+        })
+        .init();
+
+    let _span = span!(Level::DEBUG, "main").entered();
+    debug!(cmd = args.cmd.join(" "), "Will run command");
+
+    if let Some(program) = args.cmd.first() {
+        let mut cmd = std::process::Command::new(program);
+        cmd.args(args.cmd.iter().skip(1));
+        cmd.status().expect("failed to execute process");
+    }
 }
